b20d7ed2a8026f8fa56e036611cd4f535c2710e8
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-12-09 17:49:14 +0100

    json test

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Booking Form</title>
+</head>
+
+<body>
+    <form action="process_booking.php" method="post">
+        <label for="start_date">Start Date:</label>
+        <input type="date" id="start_date" name="start_date" required>
+
+        <label for="end_date">End Date:</label>
+        <input type="date" id="end_date" name="end_date" required>
+
+        <label for="room_type">Room Type:</label>
+        <select id="room_type" name="room_type" required>
+            <option value="cheap">Cheap</option>
+            <option value="medium">Medium</option>
+            <option value="expensive">Expensive</option>
+        </select>
+
+        <input type="submit" value="Book">
+    </form>
+</body>
+
+</html>
---
+++ public/process_booking.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    header('Content-Type: application/json');
+
+    $startDate = $_POST['start_date'];
+    $endDate = $_POST['end_date'];
+    $roomType = $_POST['room_type'];
+
+    $duration = (strtotime($endDate) - strtotime($startDate)) / 86400;
+    $costPerDay = ($roomType === 'expensive') ? 50 : (($roomType === 'medium') ? 30 : 15);
+    $totalCost = $duration * $costPerDay;
+
+    $bookingReceipt = [
+        "island" => "Main island",
+        "hotel" => "Centralhotellet",
+        "arrival_date" => $startDate,
+        "departure_date" => $endDate,
+        "total_cost" => strval($totalCost),
+        "stars" => "3",
+        "features" => [
+            ["name" => "sauna", "cost" => 2]
+        ],
+        "additional_info" => [
+            "greeting" => "Thank you for choosing Centralhotellet",
+            "imageUrl" => "https://upload.wikimedia.org/wikipedia/commons/e/e2/Hotel_Boscolo_Exedra_Nice.jpg"
+        ]
+    ];
+
+    echo json_encode($bookingReceipt, JSON_PRETTY_PRINT);
+    exit;
+}
